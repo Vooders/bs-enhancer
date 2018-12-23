d374866b1d02c5acc708f21c64e77eb5ced16112
Kev Wilson <kev.wilson84@gmail.com>
2018-12-23 18:12:10 +0000

    basic functionality

---
+++ enhance.js
@@ -0,0 +1,45 @@
+var categories = document.getElementsByClassName('category')
+var names = []
+
+for (let i = 0; i < categories.length; i++) {
+  var units = categories.item(i).getElementsByClassName('rootselection')
+
+  for (var k=0; k<units.length; k++) { 
+    var name = units[k].innerText.split('\n')[0].split('[')[0].trim()
+    var correctedName = (names.indexOf(name) > -1) ? name+'1' : name
+    names.push(correctedName)
+    units[k].setAttribute('id', makeId(correctedName))
+  }
+}
+
+console.log(names)
+var div = document.createElement("div")
+div.id = 'nav'
+document.getElementsByClassName('battlescribe')[1].appendChild(div)
+
+for (var i=0; i < names.length; i++) {
+  var link = document.createElement('a')
+  var linkText = document.createTextNode(names[i])
+  link.appendChild(linkText);
+  link.setAttribute('href', '#' + makeId(names[i]))
+  link.setAttribute('title', names[i])
+  console.log(link)
+  document.getElementById('nav').appendChild(link)
+}
+
+var cssId = 'bootstrap';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
+
+function makeId (name) {
+  return name.replace(/ /g, '_')
+}
