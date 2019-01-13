741a7b3d0579cb8d9db386be055ec92f9e42ce36
Kev Wilson <kev.wilson84@gmail.com>
2019-01-13 15:13:13 +0000

    Remove dist

--- dist/start.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const body = document.getElementsByClassName('battlescribe')[1]
-const head = document.getElementsByTagName('head')[0]
-if (body) {
-    console.log(body);
-    console.log(head)
-}
-//# sourceMappingURL=start.js.map
