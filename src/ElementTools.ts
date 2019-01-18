3bc02fd32c38c2b9e1058500b4a56f3f20b9a862
Kev Wilson <kev.wilson84@gmail.com>
2019-01-18 21:55:16 +0000

    Add Element tools

---
+++ src/ElementTools.ts
@@ -0,0 +1,15 @@
+export class ElementTools {
+  static hide (section: HTMLElement, id: string, buttonText: string) {
+    section.setAttribute('id', id + '_selection')
+    section.setAttribute('class', 'collapse')
+
+    var revealButton = document.createElement('button')
+    var linkText = document.createTextNode(buttonText)
+    revealButton.appendChild(linkText)
+    revealButton.setAttribute('href', `#${id}_selection`)
+    revealButton.setAttribute('data-toggle', 'collapse')
+    revealButton.setAttribute('role', 'button')
+    revealButton.style.margin = '5px'
+    return revealButton
+  }
+}
